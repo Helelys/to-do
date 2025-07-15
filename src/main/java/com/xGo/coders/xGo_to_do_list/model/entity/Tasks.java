b4f47b07b06e2c19315f9843e5136c37b6e6aeac
Almeida <talalmeida@deloitte.com>
2025-07-15 13:58:43 -0300

    subindo entity

---
+++ src/main/java/com/xGo/coders/xGo_to_do_list/model/entity/Tasks.java
@@ -0,0 +1,27 @@
+package com.xGo.coders.xGo_to_do_list.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+public class Tasks {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Title cannot be empty")
+    private String title;
+    private String description;
+    private boolean finished;
+    private LocalDateTime creationDate;
+}
---
+++ src/main/java/com/xGo/coders/xGo_to_do_list/repository/TasksRepository.java
@@ -0,0 +1,9 @@
+package com.xGo.coders.xGo_to_do_list.repository;
+
+import com.xGo.coders.xGo_to_do_list.model.entity.Tasks;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TasksRepository extends JpaRepository<Long, Tasks> {
+}
