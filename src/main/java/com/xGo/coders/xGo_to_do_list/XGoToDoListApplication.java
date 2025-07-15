ae2b3e000cea5127ca354bb2b0a819c3b77f1789
Almeida <talalmeida@deloitte.com>
2025-07-15 13:29:48 -0300

    primeiro commit

---
+++ src/main/java/com/xGo/coders/xGo_to_do_list/XGoToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.xGo.coders.xGo_to_do_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XGoToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XGoToDoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xGo/coders/xGo_to_do_list/XGoToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xGo.coders.xGo_to_do_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XGoToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
