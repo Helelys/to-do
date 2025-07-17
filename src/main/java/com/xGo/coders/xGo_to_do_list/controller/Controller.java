201be3178b148468dd88dd1fb6e142ea5d204d72
Filho <anfilho@deloitte.com>
2025-07-17 15:27:31 -0300

    Post na controller finalizado. método de conversão de dto na entidade realizado

--- src/main/java/com/xGo/coders/xGo_to_do_list/controller/Controller.java
+++ src/main/java/com/xGo/coders/xGo_to_do_list/controller/Controller.java
@@ -1,12 +1,13 @@
 package com.xGo.coders.xGo_to_do_list.controller;
 
 import com.xGo.coders.xGo_to_do_list.model.dto.TasksDTO;
+import com.xGo.coders.xGo_to_do_list.model.entity.Tasks;
 import com.xGo.coders.xGo_to_do_list.services.TaksGet;
+import com.xGo.coders.xGo_to_do_list.services.TasksPost;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +25,14 @@ public class Controller {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.ok(tasks);
+    }
+
+    private final TasksPost tasksPost;
+
+    @PostMapping("/tasks")
+    public ResponseEntity<TasksDTO> postTask(@RequestBody TasksDTO dto) {
+        Tasks convertedTask = tasksPost.postTask(dto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(convertedTask.toDTO());
     }
 }
